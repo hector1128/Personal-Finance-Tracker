ab988bf5c0404e33d6e48724517278bf5e87c356
hector1128 <hectorhoraciocordero11@gmail.com>
2024-10-18 10:52:12 -0400

    Getting started
    not sure where I'm going with this. Subject to change.

---
+++ PFT.py
@@ -0,0 +1,6 @@
+print("**PERSONAL FINANCE TRACKER**")
+
+# Classes ----------------------------------------------------------------------
+class User:
+    def __init__(self, income, groceries, eatingout, housing):
+        pass
