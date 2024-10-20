567bcfab44f8e92680cf2c6e2094f31e19bcfc3c
hector1128 <hectorhoraciocordero11@gmail.com>
2024-10-20 10:20:31 -0400

    Changes
    Added a try and except for error handling
    Added bigpayments
    Added new input to update user info

--- PFT.py
+++ PFT.py
@@ -2,31 +2,46 @@ print("**PERSONAL FINANCE TRACKER**")
 
 print("ENTER USERNAME BELOW")
 print("If you don't have an acccount, type \"new\" to sign up.")
+print("If you wish to update your information type \"update\".\n")
 username=input()
+try:
+    if username =="new":
+        print("WELCOME TO PFT v1\n")
+        budget = int(input("How much money do you have?\n"))
+        income = int(input("How much do you earn weekly?\n"))
+        groceries=int(input("How much do you usually spend on groceries?\n"))
+        transportation = int(input("How much do you spend weekly on your car/transportation?\n"))
+        housing=int(input("How much do you usually spend on housing?\n"))
+        bigpayments=int(input("Do you have any big payments coming up?\n"))
+        extra=int(input("Do you spend any extra money apart from what is stated here? If not, type \"0\""))
+        
+    if True:# Username exists:
+        print("WELCOME BACK")
 
-if username =="new":
-    print("WELCOME TO PFT v1\n")
-    budget = int(input("How much do you ha?\n"))
-    income = int(input("How much do you earn weekly?\n"))
-    groceries=int(input("How much do you usually spend on groceries?\n"))
-    eatingout = int(input("How much do you earn weekly?\n"))
-    housing=int(input("How much do you usually spend on housing?\n"))
-    extra=int(input("Do you spend any extra money apart from what is stated here? If not, type \"n\""))
+        print('TOTAL BUDGET')
 
-elif True:# Username exists:
-    print('TOTAL BUDGET')
+        print('ratio between spendatures and income')
 
-    print('How much i should spend per week')
+        print("Max amnt of money users should spend per week")
+
+    if username=="update":
+        pass
+except SyntaxError:
+    print("Invalid character. Run the program again and use valid characters.")
+
+except:
+    print("Oops. Something went wrong. Try again.")
 
 
 # Classes ----------------------------------------------------------------------
 class User:
-    def __init__(self, budget, income, groceries, eatingout, housing, extra==0):
+    def __init__(self, budget, income, groceries, eatingout, housing, bigpayments, extra==0):
         self.budget=budget
         self.income=income
         self.groceries=groceries
         self.eatingout=eatingout
         self.housing=housing
+        self.bigpayments=bigpayments
         self.extra=extra
 
 
