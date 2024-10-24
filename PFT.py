2e30fb241658efc2dfd6b8bdfd0d3380e55738df
hector1128 <hectorhoraciocordero11@gmail.com>
2024-10-24 11:25:52 -0400

    Added "new" login
    Allowed user to signup using file I/O
    Now I just need to add a way for them to access the program

--- PFT.py
+++ PFT.py
@@ -3,7 +3,6 @@ import time
 def sec(x):
     return time.sleep(x)
 
-login = {}
 
 # The user's information
 class User:
@@ -41,6 +40,7 @@ username=input()
 
 try:
     db = open("logindatabase.txt", "r")
+    
     if username in db:
         def access():
             pw = input("P: ")
@@ -48,13 +48,15 @@ try:
                 print("WELCOME BACK")
 
                 print('TOTAL BUDGET')
-                print(name.budget)
+            # print(name.budget)
                 print('ratio between spendatures and income')
 
                 print("Max amnt of money users should spend per week")
             else:
                 print("Password incorrect. Try again.")
-                access()
+        access()
+    db.close()
+    
     if username =="new":
         def register():
             db = open("logindatabase.txt", "r")
@@ -62,12 +64,6 @@ try:
             name = input("Create a username: ")
             pw = input("Create a password: ")
             pw2 = input("Re-enter your password: ")
-            if pw != pw2:
-                print("passwords do not match, try again.")
-                register()
-            if name in db:
-                print("Username already exists. Choose another one.")
-            db = open("logindatabase.txt", "a")
             u = []
             p = []
             for i in db:
@@ -75,25 +71,42 @@ try:
                 b= b.strip()
                 u.append(a)
                 p.append(b)
+            login = dict(zip(u, p))
             
-            budget = int(input("How much money do you have?\n"))
-            income = int(input("How much do you earn weekly?\n"))
-            groceries=int(input("How much do you usually spend on groceries?\n"))
-            transportation = int(input("How much do you spend weekly on your car/transportation?\n"))
-            housing=int(input("How much do you usually spend on housing?\n"))
-            bigpayments=int(input("Do you have any big payments coming up?\n"))
-            extra=int(input("Do you spend any extra money apart from what is stated here? If not, type \"0\"\n"))
-            name=User(budget, income, groceries, transportation, housing, bigpayments, extra)
+            if len(pw)<6:
+                print("Password is too short. Try again")
+                register()
+            elif pw != pw2:
+                print("passwords do not match, try again.")
+                register()
+            elif name in db:
+                print("Username already exists. Choose another one.")
+                register()
+            else:
+                db=open("logindatabase.txt", "a")
+                db.write(name + ", " + pw + "\n")
+                print("Sign up successful.")
+            db.close()
+        register()
+            
+            #budget = int(input("How much money do you have?\n"))
+            #income = int(input("How much do you earn weekly?\n"))
+            #groceries=int(input("How much do you usually spend on groceries?\n"))
+            #transportation = int(input("How much do you spend weekly on your car/transportation?\n"))
+            #housing=int(input("How much do you usually spend on housing?\n"))
+            #bigpayments=int(input("Do you have any big payments coming up?\n"))
+            #extra=int(input("Do you spend any extra money apart from what is stated here? If not, type \"0\"\n"))
+            #name=User(budget, income, groceries, transportation, housing, bigpayments, extra)
             # Save that info in a file ^^
         
     
 
-    while True:
-        if username=="update":
-            username = input("What is your username?\n")
+    #while True:
+        #if username=="update":
+           # username = input("What is your username?\n")
         # If username in files:
             # Retrieve user
-            # 
+            
 except SyntaxError:
     print("Invalid character. Run the program again and use valid characters.")
 
@@ -101,20 +114,6 @@ except:
     print("Oops. Something went wrong. Try again.")
 
 
-# Classes ----------------------------------------------------------------------
-class User:
-    def __init__(self, budget, income, groceries, transportation, housing, bigpayments, extra=0):
-        self.budget=budget
-        self.income=income
-        self.groceries=groceries
-        self.transportation=transportation
-        self.housing=housing
-        self.bigpayments=bigpayments
-        self.extra=extra
-
-user1=User(budget, income, groceries, transportation, housing, bigpayments, extra)
-
-print(user1)
 
 
 
