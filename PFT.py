36277d619b37e749995a7eb503b12f2c148e25b7
hector1128 <hectorhoraciocordero11@gmail.com>
2024-10-25 15:32:57 -0400

    worked on update
    *Stored data from personal info into a dictionary
    *Displayed it to the user
    *Just missing updat logic
    *Whatever the user wants to update from the data, they can change it and it should get uploaded to personalinfo.txt

--- PFT.py
+++ PFT.py
@@ -11,11 +11,11 @@ def user_info(username):
         groceries=int(input("How much do you usually spend on groceries?\n"))
         transportation = int(input("How much do you spend weekly on your car/transportation?\n"))
         housing=int(input("How much do you usually spend on housing?\n"))
-        bigpayments=int(input("Do you have any big payments coming up?\n"))
+        bigpayment=int(input("Do you have any big payments coming up?\n"))
         extra=int(input("Do you spend any extra money apart from what is stated here? If not, type \"0\"\n"))
         
         db = open("personalinfo.txt", "a")
-        db.write(f"{username}, {budget}, {income}, {groceries}, {transportation}, {housing}, {bigpayments}, {extra}\n")
+        db.write(f"username: {username}, budget: {budget}, income: {income}, groceries: {groceries}, transportation: {transportation}, housing: {housing}, bigpayment: {bigpayment}, extra: {extra}\n")
         db.close()
     except SyntaxError:
         print("Input numbers only.")
@@ -31,25 +31,38 @@ def home():
 
     sec(3)
 
-    print("If you don't have an acccount, type \"new\" to sign up.")
+    print("If you don't have an acccount, type **\"new\"** to sign up.")
 
     sec(1)
 
-    print("If you wish to update your information type \"update\".\n")
+    print("If you wish to update your information type **\"update\"**.\n")
 
-    username=input()
+    username=input("U: ")
+
+    sec(3)
 
     try:    
         if username =="new":
             def register():
                 db = open("logindatabase.txt", "r")
-                print("WELCOME TO PFT v1\n")
+                print("*----------------------WELCOME TO PFT v1----------------------*\n")
+
+                sec(3)
+
                 name = input("Create a username: ")
+
+                sec(1)
+
                 pw = input("Create a password: ")
+
+                sec(1)
+
                 pw2 = input("Re-enter your password: ")
+
+                sec(1)
+
                 u = []
                 p = []
-                
                 for line in db:
                     if line.strip():
                         a,b=line.split(", ")
@@ -60,12 +73,15 @@ def home():
                 
                 if len(pw)<6:
                     print("Password is too short. Try again")
+                    sec(1)
                     register()
                 elif pw != pw2:
                     print("passwords do not match, try again.")
+                    sec(1)
                     register()
                 elif name in u:
                     print("Username already exists. Choose another one.")
+                    sec(1)
                     register()
                 else:
                     db.close()
@@ -75,14 +91,33 @@ def home():
                     print(f"Sign up successful. Welcome {name}")
                     sec(3)
                     user_info(name)
-                    print("You will now be redirected to the home page.")
+                    sec(3)
+                    print("Thank you! You will now be redirected to the home page.")
                     sec(3)
                     home()
 
             register()
         
         if username=="update":
-            pass
+            username=input("What's your username??\n") 
+            print("Here's your current information:")
+            db = open("personalinfo.txt", "r")
+            lines=db.readlines()
+            db.close()
+            info = {}
+            for line in lines:
+                line.strip()
+                entries = line.split(", ")
+                for entry in entries:
+                    key, value=entry.split(": ")
+                    info[key.strip()]=value.strip()
+                for key, value in info.items():
+                    if value == username:
+                        for key, value in info.items():
+                            print(f"{key}: {value}")       
+                info.clear()
+            update = input("What do you want to change?")
+            
 
         db = open("logindatabase.txt", "r")
         users = db.readlines()
@@ -94,9 +129,11 @@ def home():
                     def access():
                         entered_pw = input("P: ")
                         if entered_pw == pw:
-                            print(f"WELCOME BACK {username}!")
+                            print(f"*----------------------------Welcome back {username}!---------------------------------*")
+                            sec(3)
                         else:
                             print("Password incorrect. Try again.")
+                            sec(1)
                             access()
                     access()
                     break
