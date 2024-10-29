51fd9e0d12137c9c6b451e58f546832f8469e629
hector1128 <hectorhoraciocordero11@gmail.com>
2024-10-29 15:19:45 -0400

    Added income logic (almost perfect)
    *Calculated the income of the user by multiplying their hourly rate by number of hours worked per week
    *Everything works, but a small issue where, username not found appears after using the update function and trying to log in

--- PFT.py
+++ PFT.py
@@ -3,27 +3,182 @@ import time
 def sec(x):
     return time.sleep(x)
 
+def access(username, pw):
+    entered_pw = input("P: ")
+    if entered_pw == pw:
+        sec(2)
+        print(f"*----------------------------------------------------Welcome back {username}!------------------------------------------------------------*")
+        sec(3)
+    else:
+        print("Password incorrect. Try again.")
+        sec(1)
+        access(username, pw)
+
+
+def update():
+    username=input("What's your username??\n")
+    sec(3) 
+    db = open("personalinfo.txt", "r")
+    lines=db.readlines()
+    db.close()
+    info = {}
+    infoinfiles=lines[:]
+    founduser=False
+
+    def updateinfo():
+        newinfo=input("What do you want to change?\n") 
+        sec(3)
+        if newinfo in info:
+            try:
+                updatevalue=int(input(f"Enter your new {newinfo} info. (Type a number)\n"))
+                sec(3)
+                if newinfo == "hoursworked":
+                    income = int(info["income"])/int(info["hoursworked"])
+                    weeklyearnings = round(income*updatevalue)
+                    info["income"]=weeklyearnings
+                info[newinfo]=updatevalue
+                inputvalue=""
+                inputvalue=", ".join(f"{key}: {value}" for key, value in info.items())
+                for i, line in enumerate(infoinfiles):
+                    if username in line:
+                        infoinfiles[i]=inputvalue+"\n"
+                        break
+                db=open("personalinfo.txt", "w")
+                for line in infoinfiles:
+                    db.write(line)
+                db.close()
+                sec(2)
+                print("Here's your updated information:\n")
+                sec(2)
+                for key, value in info.items():
+                    print(f"{key}: {value}")
+                addinfo = input("Would you like to add anything else? If so, type \"add\". Otherwise, click \"Enter\" to go back home.\n")
+                addinfo.lower()
+                if addinfo=="add":
+                    updateinfo()
+                else:
+                    print("Thank you! You will now be redirected to the home page.")
+                    sec(2)
+                    home()
+
+
+            except ValueError:
+                print("Make sure to input a number ONLY.")
+                sec(1)
+                updateinfo() 
+
+        else:
+            print("Invalid entry. Make sure you type one of the following:")
+            print("budget, income, hoursworked, groceries, transportation, housing, bigpayment, extra")
+            sec(3)
+            updateinfo()
+    checkforuser=True
+    for line in lines:
+        line = line.strip()
+        entries = line.split(", ")
+        for entry in entries:
+            key, value=entry.split(": ")
+            info[key.strip()]=value.strip()
+
+
+        for key, value in info.items():
+            if value == username:
+                checkforuser = False
+                print("Here's your current information:")
+                sec(1)
+                for key, value in info.items():
+                    print(f"{key}: {value}")
+                updateinfo()
+                break
+    if checkforuser:        
+        print("username not found. Re-type it correctly.")
+        print("If you would like to create an account, type \"create\" to sign-up. Otherwise, click Enter to try again.")
+        sec(3)
+        backhome=input()
+        if backhome=="create":
+            home()
+        else:
+            update()                        
+        info.clear()
+
+def register():
+    db = open("logindatabase.txt", "r")
+    print("*---------------------------------------------------------------------WELCOME TO PFT v1----------------------------------------------------------------------*\n")
+
+    sec(3)
+
+    name = input("Create a username: ")
+
+    sec(1)
+
+    pw = input("Create a password: ")
+
+    sec(1)
+
+    pw2 = input("Re-enter your password: ")
+
+    sec(1)
+
+    u = []
+    p = []
+    for line in db:
+        if line.strip():
+            a,b=line.split(", ")
+            b= b.strip()
+            u.append(a)
+            p.append(b)
+    login = dict(zip(u, p))
+    
+    if len(pw)<6:
+        print("Password is too short. Try again")
+        sec(1)
+        register()
+    elif pw != pw2:
+        print("passwords do not match, try again.")
+        sec(1)
+        register()
+    elif name in u:
+        print("Username already exists. Choose another one.")
+        sec(1)
+        register()
+    else:
+        db.close()
+        db=open("logindatabase.txt", "a")
+        db.write(name + ", " + pw + "\n")
+        db.close()
+        print(f"Sign up successful. Welcome {name}!")
+        sec(3)
+        user_info(name)
+        sec(3)
+        print("Thank you! You will now be redirected to the home page.")
+        sec(3)
+        home()
 #-----------------------------------------------------------------------------GETTING USER INFO(2)-------------------------------------------------------------------------
 def user_info(username):
     try:            
         print("Let's get some personal info so we can build the right tracker for YOU!")
         sec(2)
-        budget = int(input("How much money do you have?\n"))
+        print("**NOTE: INPUT A NUMBER ONLY**")
         sec(1)
-        income = int(input("How much do you earn weekly?\n"))
+        budget = int(input("How much do you have in checkings? (DOES NOT INCLUDE YOUR SAVINGS)\n"))
+        sec(1)
+        income = int(input("How much do you earn per hour?\n"))
+        sec(1)
+        hoursworked = int(input("And how many hours do you work per week?\n"))
+        income*=hoursworked
         sec(1)
         groceries=int(input("How much do you usually spend on groceries?\n"))
         sec(1)
         transportation = int(input("How much do you spend weekly on your car/transportation?\n"))
         sec(1)
-        housing=int(input("How much do you usually spend on housing?\n"))
+        housing=int(input("How much do you usually spend on housing monthly?\n"))
         sec(1)
-        bigpayment=int(input("Do you have any big payments coming up?\n"))
+        bigpayment=int(input("Do you have a big payment coming up? If so, state the due as a number. Otherwise, type \"0\"\n"))
         sec(1)
-        extra=int(input("Do you spend any extra money apart from what is stated here? If not, type \"0\"\n"))
+        extra=int(input("Do you spend any extra money weekly apart from what is stated here? If so, state the due as a number. Otherwise, type \"0\"\n"))
         
         db = open("personalinfo.txt", "a")
-        db.write(f"username: {username}, budget: {budget}, income: {income}, groceries: {groceries}, transportation: {transportation}, housing: {housing}, bigpayment: {bigpayment}, extra: {extra}\n")
+        db.write(f"username: {username}, budget: {budget}, income: {income}, hoursworked: {hoursworked}, groceries: {groceries}, transportation: {transportation}, housing: {housing}, bigpayment: {bigpayment}, extra: {extra}\n")
         db.close()
     except ValueError:
         print("Input numbers only.")
@@ -31,7 +186,8 @@ def user_info(username):
 
 #--------------------------------------------------------------------------------HOME FUNCTION(1)--------------------------------------------------------------------------
 def home():
-    print("**----------------------------------------------------------PERSONAL FINANCE TRACKER**-----------------------------------------------------------------------\n")
+    founduser=False
+    print("**----------------------------------------------------------PERSONAL FINANCE TRACKER**-----------------------------------------------------------------------**\n")
 
     sec(3)
 
@@ -50,160 +206,29 @@ def home():
     sec(3)
 
     try:    
+#---------------------------------------------------------------------------------REGISTER FUNCTION(1A)---------------------------------------------------------------------
         if username =="new":
-#---------------------------------------------------------------------------------REGISTER FUNCTION(1A)---------------------------------------------------------------------           
-            def register():
-                db = open("logindatabase.txt", "r")
-                print("*----------------------WELCOME TO PFT v1----------------------*\n")
-
-                sec(3)
-
-                name = input("Create a username: ")
-
-                sec(1)
-
-                pw = input("Create a password: ")
-
-                sec(1)
-
-                pw2 = input("Re-enter your password: ")
-
-                sec(1)
-
-                u = []
-                p = []
-                for line in db:
-                    if line.strip():
-                        a,b=line.split(", ")
-                        b= b.strip()
-                        u.append(a)
-                        p.append(b)
-                login = dict(zip(u, p))
-                
-                if len(pw)<6:
-                    print("Password is too short. Try again")
-                    sec(1)
-                    register()
-                elif pw != pw2:
-                    print("passwords do not match, try again.")
-                    sec(1)
-                    register()
-                elif name in u:
-                    print("Username already exists. Choose another one.")
-                    sec(1)
-                    register()
-                else:
-                    db.close()
-                    db=open("logindatabase.txt", "a")
-                    db.write(name + ", " + pw + "\n")
-                    db.close()
-                    print(f"Sign up successful. Welcome {name}")
-                    sec(3)
-                    user_info(name)
-                    sec(3)
-                    print("Thank you! You will now be redirected to the home page.")
-                    sec(3)
-                    home()
-
             register()
         
 #-------------------------------------------------------------------------UPDATE FUNCTION(1B)--------------------------------------------------------------------------------
         if username=="update":
-            def update():
-                username=input("What's your username??\n")
-                sec(3) 
-                db = open("personalinfo.txt", "r")
-                lines=db.readlines()
-                db.close()
-                info = {}
-                infoinfiles=lines[:]
-   
-                def updateinfo():
-                    newinfo=input("What do you want to change?\n") 
-                    sec(3)
-                    if newinfo in info:
-                        try:
-                            updatevalue=int(input(f"Enter your new {newinfo}. (Type a number)\n"))
-                            sec(3)
-                            info[newinfo]=updatevalue
-                            inputvalue=""
-                            inputvalue=", ".join(f"{key}: {value}" for key, value in info.items())
-                            for i, line in enumerate(infoinfiles):
-                                if username in line:
-                                    infoinfiles[i]=inputvalue+"\n"
-                                    break
-                            db=open("personalinfo.txt", "w")
-                            for line in infoinfiles:
-                                db.write(line)
-                            db.close()
-                        except:
-                            print("Make sure to input a number ONLY.")
-                            sec(1)
-                            updateinfo() 
-
-                    else:
-                        print("Invalid entry. Make sure you type one of the following:")
-                        print("budget, income, groceries, transportation, housing, bigpayment, extra")
-                        sec(3)
-                        updateinfo()
-                checkforuser=True
-                for line in lines:
-                    line = line.strip()
-                    entries = line.split(", ")
-                    for entry in entries:
-                        key, value=entry.split(": ")
-                        info[key.strip()]=value.strip()
-
-        
-                    for key, value in info.items():
-                        if value == username:
-                            checkforuser = False
-                            print("Here's your current information:")
-                            sec(1)
-                            for key, value in info.items():
-                                print(f"{key}: {value}")
-                            updateinfo()
-                            break
-                if checkforuser:        
-                    print("username not found. Re-type it correctly.")
-                    print("If you would like to create an account, type \"create\" to sign-up. Otherwise, click Enter to try again.")
-                    sec(3)
-                    backhome=input()
-                    if backhome=="create":
-                        register()
-                    update()                        
-                    info.clear()
-
             update()
             
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
         db = open("logindatabase.txt", "r")
         users = db.readlines()
         db.close()
-        usercheck=True
         for line in users:
             if line.strip():
                 user, pw = line.strip().split(", ")
                 if username==user:
-                    usercheck = False
-                    def access():
-                        entered_pw = input("P: ")
-                        if entered_pw == pw:
-                            print(f"*----------------------------Welcome back {username}!---------------------------------*")
-                            sec(3)
-                        else:
-                            print("Password incorrect. Try again.")
-                            sec(1)
-                            access()
-                    access()
+                    founduser=True
+                    access(username, pw)
                     break
-        if usercheck:
+        if not founduser:
             print("Username not found. Try again.")
+            sec(2)
             home()
-
-        
-            
     except SyntaxError:
         print("Invalid character. Run the program again and use valid characters.")
 
